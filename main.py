3ac9e059cca31672d154f06d868fdbfd4a925ae9
Maaz Ahmed <89376942+RageForest12@users.noreply.github.com>
2022-09-10 13:45:04 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#perimter of triangle
+
+side = input(' enter side ' )
+side = int(side)
+
+base = input("Enter the base " )
+base = int(base)
+
+side2 = input('enter side 2')
+side2 = int(side2)
+
+#calculating the area
+
+final = side +side2 + base
+final = str(final)
+
+
+print("The answer is " + final)
